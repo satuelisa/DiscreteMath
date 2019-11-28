d82d88620dd39cb7c5c2e877b6c2e6ad964b64e0
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-27 18:26:54 -0600

    Definir un grafo en Python3
    Una clase para crear un grafo simple no dirigido (opcionalmente ponderado)

---
+++ homework/grafo.py
@@ -0,0 +1,26 @@
+class Grafo:
+ 
+    def __init__(self):
+        self.V = set() # un conjunto
+        self.E = dict() # un mapeo de pesos de aristas
+        self.vecinos = dict() # un mapeo
+ 
+    def agrega(self, v):
+        self.V.add(v)
+        if not v in self.vecinos: # vecindad de v
+            self.vecinos[v] = set() # inicialmente no tiene nada
+ 
+    def conecta(self, v, u, peso=1):
+        self.agrega(v)
+        self.agrega(u)
+        self.E[(v, u)] = self.E[(u, v)] = peso # en ambos sentidos
+        self.vecinos[v].add(u)
+        self.vecinos[u].add(v)
+ 
+    def complemento(self):
+        comp= Grafo()
+        for v in self.V:
+            for w in self.V:
+                if v != w and (v, w) not in self.E:
+                    comp.conecta(v, w, 1)
+        return comp
