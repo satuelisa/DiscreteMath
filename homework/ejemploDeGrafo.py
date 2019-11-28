e9889f21d48579a32ad8a4dd1baa835accf3f33f
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-27 18:28:48 -0600

    Ejemplo de uso de la clase Grafo
    Requiere la presencia del archivo grafo.py al ejecutar

---
+++ homework/ejemploDeGrafo.py
@@ -0,0 +1,11 @@
+from grafo import Grafo
+G = Grafo()
+G.conecta('a', 'b', 5)
+G.conecta('a', 'c', 7)
+G.conecta('b', 'c', 2)
+G.conecta('c', 'd', 4)
+print(G.vecinos['a'])
+print(G.V)
+print(G.E)
+G2 = G.complemento()
+print(G2.E)
