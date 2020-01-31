4b219066a03e8f8975244a0c9c851b642e9d40af
SatuElisa <elisa.schaeffer@gmail.com>
2020-01-31 10:48:17 -0600

    Mejoras de consistencia y un ejemplo de entrada

--- homework/fibo.py
+++ homework/fibo.py
@@ -1,17 +1,19 @@
-def fSecuencia(cuantos):
-    listado = [0, 1] # fragmento inicial                                                                                  
-    while len(listado) < cuantos: # extendemos al largo deseado                                                           
-        listado.append(listado[-1] + listado[-2]) # sumando los dos elementos al final                                    
-    return listado # regresamos resultado                                                                                 
+def fSecuencia(k): # genera los k primeros elementos
+    listado = [0, 1] # fragmento inicial
+    while len(listado) < k: # extendemos al largo deseado
+        listado.append(listado[-1] + listado[-2]) # suma de los dos al final
+    return listado # regresamos resultado
 
-def fibo(n):
+def fibo(n): # genera F_n
     if n < 2:
         return n
     a, b = 0, 1
-    while n > 2:
+    while n > 1:
         a, b, n = b, a + b, n - 1
     return b
 
-n = 18
-secuencia = fSecuencia(n) # consultamos la secuencia de veinte elementos                                                  
-assert secuencia[-1] == fibo(n)
+secuencia = fSecuencia(20) # consultamos veinte elementos
+assert secuencia[-1] == fibo(19) # al final viene F_19 ya que F_0 es primero
+print(secuencia)
+n = int(input("Dame un n: "))
+print(f'F_n vale {fibo(n)}')
