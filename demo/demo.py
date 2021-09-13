b995ecc390865d53229c2036d15d5ff6b5c2cf25
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-09-13 16:49:00 -0500

    This should be done now

--- demo/demo.py
+++ demo/demo.py
@@ -1,8 +1,9 @@
 import operator # for getting extrema from dictionaries
+from time import time
 from sys import argv # read command-line parameters
 import matplotlib.pyplot as plt # for the drawings
 from collections import defaultdict # easy storage
-from math import sqrt, log, ceil, exp # mathematical routines
+from math import sqrt, log, ceil, exp, fabs # mathematical routines
 from random import random, shuffle, randint # pseudo-random number generation
 from networkx import Graph, draw, relabel_nodes # graph representation (object-oriented)
 
@@ -10,12 +11,22 @@ n = None
 try:
     n = int(argv[1]) # how many cities does our TSP have
 except:
-    n = 4 # a small default
+    n = 5 # a small default
 assert n > 2 # we need at least three for this to make sense
 print(f'Working with a TSP of {n} cities')
-w = int(n * sqrt(n)) 
-radius = 1 / n
+
 unit = 8
+width = int(n * sqrt(n))
+wunit = (width / n) * unit
+radius = 1 / n
+magn = int(ceil(log(n, 2)))
+significant = 0.01
+gns = max(50, 300 - 20 * int(log(n, 2))) if n <= 30 else 20 # node size for graphs
+tns = max(150, 400 - 20 * int(log(n, 2))) if n < 5 else 20 # node size for trees
+
+def timestamp(start):
+    ms = 1000 * (time() - start)
+    print(f'That took {ms:.0f} ms of runtime')
 
 # step 1) represent the complete graph with euclidean-distance weights
 
@@ -49,37 +60,40 @@ while v < n: # add a total of n vertices called 0, 1, 2, ..., n - 1
         
 low = sqrt(2) # the highest possible Euclidean distance in a unit square
 high = 0 # the lowest possible distance
+edgecosts = dict()
 for v in gpos: # for each vertex
     for w in gpos: # for each other vertex
         if v == w:
             continue # skip self
-        G.add_edge(v, w, cost = eucl(gpos[v], gpos[w]))
+        d = eucl(gpos[v], gpos[w])
+        G.add_edge(v, w, cost = d)
+        edgecosts[(v, w)] = d
+        edgecosts[(w, v)] = d
         low = min(low, distance) # figure out the minimum edge cost
         high = max(high, distance) # as well as the maximum
-# view the min-max costs at two-decimal precision
+# view the min-max edgecosts at two-decimal precision
 print(f'The minimum single-edge cost is {low:.2f} and the maximum is {high:.2}')
 
 # first we set up how we want the visualization to look
-opt = { 'node_color': 'white', # white vertex color
-        'font_color': 'black' # vertex label font color
-}
+opt = { 'node_color': 'white', 'font_color': 'black' }
 opt['with_labels'] = n <= 30
-opt['node_size'] = max(100, 300 - 10 * int(log(n, 2)))
-opt['width'] = max(4 - int(ceil(log(n, 10))), 1)
-costs = [ data['cost'] for v, w, data in G.edges(data = True) ]
-worst = sum(costs) # no solution could ever cost more than ALL the edges
+opt['node_size'] = gns
+opt['width'] = max(4 - magn, 1)
+worst = sum(edgecosts.values()) / 2 # no solution could ever cost more than ALL the edges
+
 plt.rcParams['figure.figsize'] = (unit, unit) # big figure
 if n <= 100:
     fig, ax = plt.subplots() # create an image to draw onto
     draw(G, pos = gpos, # place each vertex in the unit square using its coordinates
          edge_cmap = plt.get_cmap('Oranges'), # use tones of orange to color the edges
-         edge_color = costs, # the tone of each depends on its cost
+         edge_color = [ edgecosts[(v, w)] for v, w in G.edges() ], # the tone of each depends on its cost
          **opt) # use the options above
     ax.set_facecolor('black') # make it a black background
     fig.set_facecolor('black')
     ax.axis('off') # no axis needed
     plt.savefig(f'graph{n}.png')
     plt.close()
+
 
 class Node: # now, consider all the possible visitation orders as a tree 
 
@@ -96,9 +110,7 @@ class Node: # now, consider all the possible visitation orders as a tree
         self.total = 0
         self.label = None
         
-    def adjust(self): # set values for path, cost, height
-        self.path = self.parent.path + '.' if self.parent is not None else ''
-        self.path += str(self.value)
+    def adjust(self): # set values for height
         tallest = [ child.adjust() for child in self.children ] if self.children is not None else [ 0 ]
         self.height = 1 + max(tallest) # leaves have height one 
         return self.height
@@ -115,39 +127,38 @@ class Node: # now, consider all the possible visitation orders as a tree
         above = self.parent.present() if self.parent is not None else set()
         return { self.value } | above
 
-    def permute(self, values, source, root):
-        if self.parent is not None:
-            data = source.get_edge_data(self.value, self.parent.value)
-            self.cost = data['cost']
-            self.total = self.cost + self.parent.cost
+    def permute(self, values, source, root, best):
+        if self.parent is not None: # not the root
+            self.path = self.parent.path + '.' + str(self.value)
+            # the cost of the edge from the parent to this one in the graph
+            self.cost = source[(self.value, self.parent.value)]
+            self.total = self.cost + self.parent.total
+        else:
+            self.path = str(self.value)
         pending = sorted(list(values - self.present())) # skip those already present
         if len(pending) == 0: # nothing left (a leaf)
-            self.total += source.get_edge_data(self.value, root.value)['cost'] # close the loop
-            return 
+            closing = source[(self.value, root.value)]
+            self.total += closing # close the loop
+            return min(self.total, best)
         d = self.depth + 1 # the children will be one level deeper
         self.children = [ Node(value, parent = self, depth = d) for value in pending ]
         below = values - { self.value }
         for child in self.children: # continue recursively
-            child.permute(below, source, root)
+            best = min(best, child.permute(below, source, root, best))
+        return best
 
-    def position(self, ltarget, ptarget, xr, yr = (0, 1), curr = None, total = None): # position in a unit square as well 
+    def position(self, ltarget, ptarget, xr, y, dy):
         self.label = f'{self.path} = {self.total:.2f}' # make a label with total cost
         ltarget[self.path] = self.label 
         (xlow, xhigh) = xr
         self.x = (xlow + xhigh) / 2 # horizontally in the middle
-        (ylow, yhigh) = yr
-        self.y = yhigh # vertically on the top
+        self.y = y # vertical position
         ptarget[self.label] = (self.x, self.y) # associate positions to the labels
         if self.children is not None:
             xspan = xhigh - xlow
             xa = xspan / len(self.children) # horizontal allocation per child
-            yspan = yhigh - ylow
-            yp = curr / total if curr is not None and total is not None else (self.height - 1) / self.height
-            ya = yp * yspan
-            ytop = ylow + ya
             for child in self.children:
-                child.position(ltarget, ptarget, (xlow, xlow + xa), (ylow, ytop),
-                               curr - 1 if curr is not None else None, total)
+                child.position(ltarget, ptarget, (xlow, xlow + xa), y - dy, dy)
                 xlow += xa
 
     def join(self, graph): # add nodes as vertices to draw with networkx
@@ -172,28 +183,29 @@ class Node: # now, consider all the possible visitation orders as a tree
             return min([ child.cheapest(best) for child in self.children ])
 
 opt['with_labels'] = n < 5
+opt['font_color'] = 'cyan'
+opt['node_size'] = tns
 if n < 10: # too slow for larger graphs
     # note that it does not matter where we start since it is a cycle
-    root = Node(0) # we can start at zero w.l.o.g.
-    root.permute({ v for v in range(n) }, G, root)
-    root.adjust()
+    start = time()
+    groot = Node(0) # we can start at zero w.l.o.g.
+    gbest = groot.permute({ v for v in range(n) }, edgecosts, groot, worst)
+    print(f'The cheapest (exhaustive) route edgecosts {gbest:.2f}')
+    timestamp(start)
+    groot.adjust()
     T = Graph() # we make a graph of it so we can draw it the same way
-    root.join(T) # create vertices to represent the nodes
+    groot.join(T) # create vertices to represent the nodes
     labels = dict()
     tpos = dict()
-    wunit = (w / n) * unit
     plt.rcParams['figure.figsize'] = (wunit, unit) # big figure
-    root.position(labels, tpos, (0, w)) # label and position the nodes with
+    groot.position(labels, tpos, (0, w), 1, 1 / (n + 1)) # label and position the nodes with
     L = relabel_nodes(T, labels)
-    root.connect(L) # add edges to represent the branches and match the costs
-    gbest = root.cheapest(worst)
-    print(f'The cheapest (exhaustive) route costs {gbest:.2f}')
+    groot.connect(L) # add edges to represent the branches and match the edgecosts
     if n < 7: # makes no sense for large n to draw the trees
-        lcosts = [ data['cost'] for v, w, data in L.edges(data = True) ]
         fig, ax = plt.subplots()
         draw(L, pos = tpos, # using the tree positions
              edge_cmap = plt.get_cmap('Oranges'),
-             edge_color = lcosts, **opt) 
+             edge_color = [ data['cost'] for v, w, data in L.edges(data = True) ], **opt) 
         ax.set_facecolor('black')
         fig.set_facecolor('black')
         plt.savefig(f'tree{n}.png')
@@ -216,39 +228,45 @@ class Smart(Node):
         
     def permute(self, values, source, root, best): # redefine how to proceed
         if self.parent is not None:
-            data = source.get_edge_data(self.value, self.parent.value)
-            self.cost = data['cost']
-            self.total = self.cost + self.parent.cost
+            self.path = self.parent.path + '.' + str(self.value)
+            self.cost = source[(self.value, self.parent.value)]
+            self.total = self.cost + self.parent.total
             if self.total > best: # already more expensive, makes no sense to continue
+                self.path += str(self.value) # leave early
                 return best # this becomes an incomplete route if not a leaf yet
+        else:
+            self.path = str(self.value)
         pending = sorted(list(values - self.present())) # skip those already present
         if len(pending) == 0: # nothing left (a leaf)
+            closing = source[(self.value, root.value)]
+            self.total += closing
             self.complete = True
-            self.total += source.get_edge_data(self.value, root.value)['cost'] # close the loop            
-            return min(best, self.total) # possibly best, update if so            
-        else:
-            d = self.depth + 1 # the children will be one level deeper
-            self.children = [ Smart(value, parent = self, depth = d) for value in pending ]
-            below = values - { self.value }
-            return min(best, min([ child.permute(below, source, root, best) for child in self.children ]))
+            return min(self.total, best)
+        d = self.depth + 1 
+        self.children = [ Smart(value, parent = self, depth = d) for value in pending ]
+        below = values - { self.value }
+        for child in self.children: 
+            best = min(best, child.permute(below, source, root, best))
+        return best
 
 opt['font_color'] = 'cyan'
 opt['font_size'] = 12
-if n < 10: 
+if n < 10:
+    start = time()
     sroot = Smart(0)
-    pbest = sroot.permute({ v for v in range(n) }, G, root, worst)
-    print(f'The cheapest (pruned) route costs {pbest:.2f}')
-    assert gbest == pbest
+    sbest = sroot.permute({ v for v in range(n) }, edgecosts, sroot, worst)
+    print(f'The cheapest (pruned) route edgecosts {sbest:.2f}')
+    timestamp(start)
+    assert fabs(gbest - sbest) < significant
     sroot.adjust()
     P = Graph() 
     sroot.join(P)
     h = sroot.height
-    sroot.position(labels, tpos, (0, w), curr = h - 1, total = h) # label and position the nodes with
+    sroot.position(labels, tpos, (0, w), 1, 1 / (n + 1)) # label and position the nodes with
     Q = relabel_nodes(P, labels)
-    sroot.connect(Q) # add edges to represent the branches and match the costs
+    sroot.connect(Q) # add edges to represent the branches and match the edgecosts
     del opt['node_color']
     if n < 7: 
-        qcosts = [ data['cost'] for v, w, data in Q.edges(data = True) ]
         complete = dict()
         sroot.role(complete)
         nodecolor = [ complete[v] for v in Q.nodes() ] # intermediate, full solution or pruned 
@@ -257,7 +275,7 @@ if n < 10:
              node_color = nodecolor,
              cmap = plt.get_cmap('Set1'),
              edge_cmap = plt.get_cmap('Oranges'),
-             edge_color = qcosts, **opt) 
+             edge_color = [ data['cost'] for v, w, data in Q.edges(data = True) ], **opt) 
         ax.set_facecolor('black')
         fig.set_facecolor('black')
         ax.axis('off')
@@ -278,30 +296,31 @@ def cost(G, route):
         acc += d['cost']
         p += 1
     return acc
-    
+
+start = time()
 route = [ v for v in range(n) ]
-replicas = 10 * int(ceil(log(n, 2)))
+replicas = 10 * magn
 rbest = worst
 for replica in range(replicas): # try several times
     shuffle(route) # a new permutation
     cycle = route + [ route[0] ] # close the cycle
     rbest = min(rbest, cost(G, cycle))
-print(f'The cheapest random walk costs {rbest:.2f} (over {replicas} attempts)')
+print(f'The cheapest random walk edgecosts {rbest:.2f} (over {replicas} attempts)')
+timestamp(start)
 
 # so this is pretty fast but quite bad; we need a better approach
 # let us compute a minimum spanning tree 
+start = time()
 mst = set()
-costs = dict() # store candidate edges
-for v, w, data in G.edges(data = True):
-    costs[(v, w)] = data['cost'] 
 components = dict() # connected-component storage
 mstcost = 0 # cost accumulator
 neighbors = defaultdict(set)
-while len(costs) > 0: # Kruskal's algorithm for MST
-    cheapest = min(costs.items(), key = operator.itemgetter(1))[0]
+candidates = edgecosts.copy()
+while len(candidates) > 0: # Kruskal's algorithm for MST
+    cheapest = min(candidates.items(), key = operator.itemgetter(1))[0]
     (v, w) = cheapest
-    value = costs[cheapest]
-    del costs[cheapest] # remove the processed edges
+    value = edgecosts[cheapest]
+    del candidates[cheapest] # remove the processed edges
     connected = components.get(v, {v})
     if w not in connected:
         mst.add(cheapest) # use this edge
@@ -313,8 +332,10 @@ while len(costs) > 0: # Kruskal's algorithm for MST
             break
         for u in combination:
             components[u] = combination
-print(f'The minimum spanning tree costs {mstcost:.2f}')
+print(f'The minimum spanning tree edgecosts {mstcost:.2f}')
+timestamp(start)
 plt.rcParams['figure.figsize'] = (unit, unit) # restore some of these
+opt['node_size'] = gns
 opt['node_color'] = 'white' 
 opt['with_labels'] = n <= 30
 opt['font_color'] = 'black'
@@ -322,11 +343,10 @@ M = G.copy() # make a copy
 removed = G.edges() - mst
 M.remove_edges_from(removed)
 if n <= 100:
-    mcosts = [ data['cost'] for v, w, data in M.edges(data = True) ]
     fig, ax = plt.subplots()
     draw(M, pos = gpos, 
          edge_cmap = plt.get_cmap('Oranges'), 
-         edge_color = mcosts, 
+         edge_color = [ edgecosts[(v, w)] for v, w in M.edges() ], 
          **opt) 
     ax.set_facecolor('black')
     fig.set_facecolor('black')
@@ -346,12 +366,16 @@ def dfs(v):
     for w in cand:
         dfs(w)
         route.append(v)
+start = time()
 dfs(0)
 if n <= 10:
     print('Back and forth', route)
 bfcost = cost(G, route)
-print(f'The back-and-forth MST costs {bfcost:.2f}')
+print(f'The back-and-forth MST edgecosts {bfcost:.2f}')
+timestamp(start)
+
 # we need to straighten out the repeated visits
+start = time()
 prev = 0 # start at zero again
 straight = [ 0 ]
 used = set()
@@ -370,16 +394,16 @@ if n <= 10:
     print('Straighted out', straight)
 assert len(straight) == n + 1
 scost = cost(G, straight)
-print(f'The straightened-out MST cycle costs {scost:.2f}')
+print(f'The straightened-out MST cycle edgecosts {scost:.2f}')
+timestamp(start)
 
 S = G.copy() # make a copy of the graph
 S.remove_edges_from(G.edges() - used)
 if n <= 100:
-    scosts = [ data['cost'] for v, w, data in S.edges(data = True) ]
     fig, ax = plt.subplots()
     draw(S, pos = gpos, 
          edge_cmap = plt.get_cmap('Oranges'), 
-         edge_color = scosts, 
+         edge_color = [ edgecosts[(v, w)] for v, w in S.edges() ], 
          **opt) 
     ax.set_facecolor('black')
     fig.set_facecolor('black')
@@ -388,11 +412,6 @@ if n <= 100:
     plt.close()
 
 # now, some of those edges are plain dumb choices, right?
-
-inverse = dict()
-for (v, w) in costs: # symmetry
-    inverse[(w, v)] = costs[(v, w)]
-costs = { **costs, **inverse } # merge
 
 def roll(route):
     n = len(route)
@@ -406,21 +425,23 @@ def roll(route):
 # local search
 def twoopt(route):
     n = len(route)
-    while True: # pick two random cuts
-        f = randint(1, n - 1)
-        s = randint(1, n - 1)
-        f, s = min(f, s), max(f, s)
-        if f + 2 < s:
-            start = route[:f]
-            middle = route[f:s] # to invert
-            finish = route[s:]
-            return start + middle[::-1] + finish
-
+    f = randint(1, n - 2) # first cut
+    s = randint(f + 1, n - 1) # second cut
+    start = route[:f]
+    middle = route[f:s] # to invert
+    finish = route[s:]
+    assert len(start) > 0 and len(middle) > 0 and len(finish) > 0
+    return start + middle[::-1] + finish
+        
 # simulated annealing
+start = time()
 T = 1000
-cooling = 0.9995
+eps = 0.0001
+cooling = 0.999
 stalled = 0
-maximum = 500
+maximum = 200 * magn
+stuck = magn
+stable = 0
 current = straight
 cheapest = straight.copy()
 lcost = ccost = scost
@@ -429,15 +450,22 @@ while stalled < maximum:
     modified = twoopt(current)
     assert len(modified) == n + 1
     mcost = cost(G, modified)
+    d = ccost - mcost
     if mcost < lcost: # a new low (a good thing here)
         print(f'New low at {mcost:.2f} on iteration {i} at temp {T:.2f}')
         cheapest = modified.copy()
         lcost = mcost
-    d = ccost - mcost
+        if d > eps:
+            stable += 1
+            if stable > stuck:
+                stalled = maximum
+                continue
+        else:
+            stable = 0 # notable improvement
     threshold = exp(d / T) # probability
     i += 1
     if random() < threshold: # accept
-        current = roll(modified)
+        current = roll(modified) # change the start position (for varying 2-opt)c
         assert current[0] == current[-1]
         ccost = mcost
         if d > 0: # it was better
@@ -445,7 +473,8 @@ while stalled < maximum:
             stalled = 0 
             continue
     stalled += 1
-
+timestamp(start)
+    
 if n <= 100:
     final = set()
     for p in range(n):
@@ -455,11 +484,10 @@ if n <= 100:
         final.add((b, a))
     F = G.copy() # make a copy of the graph
     F.remove_edges_from(G.edges() - final)
-    scosts = [ data['cost'] for v, w, data in F.edges(data = True) ]
     fig, ax = plt.subplots()
     draw(F, pos = gpos, 
          edge_cmap = plt.get_cmap('Oranges'), 
-         edge_color = scosts, 
+         edge_color = [ edgecosts[(v, w)] for v, w in F.edges()  ],
          **opt) 
     ax.set_facecolor('black')
     fig.set_facecolor('black')
---
+++ demo/demo.sh
@@ -0,0 +1,8 @@
+python3 demo.py 4 > log4.txt
+python3 demo.py 6 > log6.txt
+python3 demo.py 8 > log8.txt
+python3 demo.py 10 > log10.txt
+python3 demo.py 20 > log20.txt
+python3 demo.py 30 > log30.txt
+python3 demo.py 100 > log100.txt
+python3 demo.py 200 > log200.txt
